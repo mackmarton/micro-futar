3ab6ef2c1e52e5a5bdcc265767cdd9a656d5a887
Marton Mack <mackmarton@edu.bme.hu>
2025-10-04 10:42:04 +0200

    DB initialisation fixed

--- init-multiple-databases.sh
+++ init-multiple-databases.sh
@@ -11,6 +11,11 @@ function create_user_and_database() {
         CREATE DATABASE $database;
         GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
 EOSQL
+
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$database" <<-EOSQL
+        GRANT ALL PRIVILEGES ON SCHEMA public TO $database;
+        ALTER SCHEMA public OWNER TO $database;
+EOSQL
 }
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
