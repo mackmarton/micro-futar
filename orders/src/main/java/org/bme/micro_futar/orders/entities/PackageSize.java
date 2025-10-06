a613731b9607bad1c7370b384690737c9d9afd09
Marton Mack <mackmarton@edu.bme.hu>
2025-10-06 09:37:30 +0200

    Deleted package size enum and created entity instead

---
+++ orders/src/main/java/org/bme/micro_futar/orders/entities/PackageSize.java
@@ -0,0 +1,19 @@
+package org.bme.micro_futar.orders.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class PackageSize {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String name;
+    public Double maxLength;
+}
--- shared/src/main/java/enums/PackageSize.java
+++
@@ -1,9 +0,0 @@
-package enums;
-
-public enum PackageSize {
-    XS, //max 15cm
-    S, //max 30cm
-    M, //max 45cm
-    L, //max 60cm
-    XL //max 80cm
-}
