0b9a58dcd45a76c6e9a61192040e8513dfcf071c
Marton Mack <mackmarton@edu.bme.hu>
2025-11-01 10:55:52 +0100

    Using MockitoBean

--- orders/src/test/java/org/bme/micro_futar/orders/controllers/ShipmentControllerTests.java
+++ orders/src/test/java/org/bme/micro_futar/orders/controllers/ShipmentControllerTests.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
@@ -32,7 +32,7 @@ class ShipmentControllerTests {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private CountryPriceService countryPriceService;
 
     @Test
