5ca1c2a171fff4109b855ca980d2fa57bda9ae41
Marton Mack <mackmarton@edu.bme.hu>
2025-11-01 10:50:43 +0100

    Testcontainers working

--- orders/src/test/java/org/bme/micro_futar/orders/TestcontainersConfiguration.java
+++ orders/src/test/java/org/bme/micro_futar/orders/TestcontainersConfiguration.java
@@ -1,25 +1,33 @@
 package org.bme.micro_futar.orders;
 
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.lifecycle.Startables;
 import org.testcontainers.utility.DockerImageName;
 
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+class TestcontainersConfiguration implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    private static final String KAFKA_IMAGE = "apache/kafka-native:latest";
+    private static final String POSTGRES_IMAGE = "postgres:latest";
 
-	@Bean
-	@ServiceConnection
-	KafkaContainer kafkaContainer() {
-		return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
-	}
+    private static final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse(KAFKA_IMAGE));
+    private static final PostgreSQLContainer<?> postgreSQL = new PostgreSQLContainer<>(DockerImageName.parse(POSTGRES_IMAGE));
 
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
-	}
+    static {
+        Startables.deepStart(kafka, postgreSQL).join();
 
+        System.setProperty("SPRING_PROFILE", "local");
+        System.setProperty("SERVER_PORT", "8080");
+        System.setProperty("DATASOURCE_URL", postgreSQL.getJdbcUrl());
+        System.setProperty("DATASOURCE_USERNAME", postgreSQL.getUsername());
+        System.setProperty("DATASOURCE_PASSWORD", postgreSQL.getPassword());
+        System.setProperty("KAFKA_BOOTSTRAP_SERVERS", kafka.getBootstrapServers());
+        System.setProperty("KEYCLOAK_URL", "url");
+    }
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        // can be empty
+    }
 }
