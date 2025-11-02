fa299b8147ce619f5ca8c4e26431449fc216545a
Marton Mack <mackmarton@edu.bme.hu>
2025-11-02 11:03:16 +0100

    Timeout for db startup

--- orders/src/test/java/org/bme/micro_futar/orders/TestcontainersConfiguration.java
+++ orders/src/test/java/org/bme/micro_futar/orders/TestcontainersConfiguration.java
@@ -3,16 +3,22 @@ package org.bme.micro_futar.orders;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.lifecycle.Startables;
 import org.testcontainers.utility.DockerImageName;
+
+import java.time.Duration;
 
 class TestcontainersConfiguration implements ApplicationContextInitializer<ConfigurableApplicationContext> {
     private static final String KAFKA_IMAGE = "apache/kafka-native:latest";
     private static final String POSTGRES_IMAGE = "postgres:latest";
 
     private static final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse(KAFKA_IMAGE));
-    private static final PostgreSQLContainer<?> postgreSQL = new PostgreSQLContainer<>(DockerImageName.parse(POSTGRES_IMAGE));
+    private static final PostgreSQLContainer<?> postgreSQL = new PostgreSQLContainer<>(DockerImageName.parse(POSTGRES_IMAGE))
+            .waitingFor(Wait.forListeningPort())
+            .waitingFor(Wait.forLogMessage(".*database system is ready to accept connections.*\\n", 2))
+            .withStartupTimeout(Duration.ofMinutes(2));
 
     static {
         Startables.deepStart(kafka, postgreSQL).join();
