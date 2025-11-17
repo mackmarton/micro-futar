0fbdc9bcfe577365968313a5444a6622638c76fe
Marton Mack <mackmarton@edu.bme.hu>
2025-11-17 09:02:31 +0100

    Removed unnecesary DepoShipment

--- courier/src/main/java/org/bme/micro_futar/courier/repositories/DepoShipmentRepository.java
+++
@@ -1,7 +0,0 @@
-package org.bme.micro_futar.courier.repositories;
-
-import org.bme.micro_futar.courier.entities.DepoShipment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepoShipmentRepository extends JpaRepository<DepoShipment, Long> {
-}
--- logistics/src/main/java/org/bme/micro_futar/logistics/repositories/DepoShipmentRepository.java
+++
@@ -1,9 +0,0 @@
-package org.bme.micro_futar.logistics.repositories;
-
-import org.bme.micro_futar.logistics.entities.DepoShipment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepoShipmentRepository extends JpaRepository<DepoShipment, Long> {
-}
