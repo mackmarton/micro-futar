29bd4ec0ed6633bdae2342590569242dab6054a1
Marton Mack <mackmarton@edu.bme.hu>
2025-11-23 13:16:36 +0100

    Deleted ShipmentRouteCourier

--- tracking/src/main/java/org/bme/micro_futar/tracking/repositories/ShipmentRouteCourierRepository.java
+++
@@ -1,8 +0,0 @@
-package org.bme.micro_futar.tracking.repositories;
-
-import org.bme.micro_futar.tracking.entities.ShipmentRouteCourier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShipmentRouteCourierRepository extends JpaRepository<ShipmentRouteCourier, Long> {
-}
-
