2bdc5613c59cbeee97c560c14515b240af0c4422
Marton Mack <mackmarton@edu.bme.hu>
2026-01-07 22:04:28 +0100

    Added keycloak setup to logistics

---
+++ logistics/src/main/java/org/bme/micro_futar/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package org.bme.micro_futar.config;
+
+import lombok.RequiredArgsConstructor;
+import org.bme.micro_futar.config.auth.CustomAccessDeniedHandler;
+import org.bme.micro_futar.config.auth.CustomAuthenticationEntryPoint;
+import org.bme.micro_futar.config.auth.JwtAuthConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthConverter jwtAuthConverter;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll());
+        http.oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter))
+                .authenticationEntryPoint(customAuthenticationEntryPoint)
+        );
+        http.sessionManagement(s -> s
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.exceptionHandling(exception -> exception
+                .accessDeniedHandler(customAccessDeniedHandler)
+                .authenticationEntryPoint(customAuthenticationEntryPoint)
+        );
+
+        return http.build();
+    }
+}
