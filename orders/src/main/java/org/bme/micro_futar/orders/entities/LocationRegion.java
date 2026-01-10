b48e516d559861b20414b56a25c8b06613b4ae76
Marton Mack <mackmarton@edu.bme.hu>
2026-01-10 20:49:48 +0100

    Removed Generated value where it was not needed

--- orders/src/main/java/org/bme/micro_futar/orders/entities/LocationRegion.java
+++ orders/src/main/java/org/bme/micro_futar/orders/entities/LocationRegion.java
@@ -1,8 +1,6 @@
 package org.bme.micro_futar.orders.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LocationRegion {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
