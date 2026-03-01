1e429accefafb3b8748791ddd6aac02658542d69
Marton Mack <mackmarton@edu.bme.hu>
2026-03-01 21:48:09 +0100

    added spring api gw

---
+++ api_gateway/src/main/java/org/bme/micro_futar/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.bme.micro_futar.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
