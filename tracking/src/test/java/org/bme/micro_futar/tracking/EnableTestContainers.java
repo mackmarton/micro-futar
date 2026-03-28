1474baee92c89440cc6b05f88dc1202002afbef6
Marton Mack <mackmarton@edu.bme.hu>
2026-03-28 19:38:54 +0100

    Added testconfig for tracking

---
+++ tracking/src/test/java/org/bme/micro_futar/tracking/EnableTestContainers.java
@@ -0,0 +1,16 @@
+package org.bme.micro_futar.tracking;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Testcontainers(disabledWithoutDocker = true)
+@ContextConfiguration(initializers = TestcontainersConfiguration.class)
+public @interface EnableTestContainers {
+}
